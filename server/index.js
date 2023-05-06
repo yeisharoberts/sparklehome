0615709ff3d445e731d08558af65e3f4a2c57839
yeisharoberts <62228994+yeisharoberts@users.noreply.github.com>
2023-05-06 16:32:44 +0800

    'updateIndex'

--- server/index.js
+++ server/index.js
@@ -27,18 +27,14 @@ const io = new Server(server, {
     }
 })
 
-const _dirname = path.dirname('')
-const buildPath = path.join(_dirname, '../client/build')
+const buildPath = path.join(__dirname, '../client/build');
 
-app.use(express.static(buildPath))
+app.use(express.static(buildPath));
 
-app.get('/*', function(req, res){
-    res.sendFile(
-        path.join(_dirname, '../client/build/index.html'),
-        function(err){
-            if (err){
-                res.status(500).send(err);
-            }
-        }
-    );
-})
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(buildPath, 'index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
