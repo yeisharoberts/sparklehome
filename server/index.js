9a813db8134ebf7f5134c0deb5425bd2681694b5
yeisharoberts <62228994+yeisharoberts@users.noreply.github.com>
2023-05-06 10:40:19 +0800

    deploy

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http')
+const Server = require('socket.io').Server
+const app = express()
+const path = require('path')
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    // allowing the cookie to be enabled
+    credentials: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(5001, () => {
+    console.log("server is now running on port 5001")
+});
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin:'*'
+    }
+})
+
+const _dirname = path.dirname('')
+const buildPath = path.join(_dirname, '../client/build')
+
+app.use(express.static(buildPath))
+
+app.get('/*', function(req, res){
+    res.sendFile(
+        path.join(_dirname, '../client/build/index.html'),
+        function(err){
+            if (err){
+                res.status(500).send(err);
+            }
+        }
+    );
+})
